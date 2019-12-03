5af56a42a37900021c766835f2fc5cdea845a031
lulu <844577557@qq.com>
2019-12-04 07:47:07 +0800

    1

--- python练习/python练习(小程序)/qq头像加数字角标/修改头像.py
+++ python练习/python练习(小程序)/qq头像加数字角标/修改头像.py
@@ -1,1 +1,14 @@
-import 
+from PIL import Image, ImageDraw, ImageFont
+
+""" 
+def add_num(img):
+    img = Image.open("/Users/lixiuwen/Downloads/wallhaven-39gogv.jpg")
+    img.show()
+
+    return 0 """
+
+
+if __name__ == '__main__':
+    image = Image.open('/Users/lixiuwen/Downloads/wallhaven-39gogv.jpg')
+
+    image.show()
