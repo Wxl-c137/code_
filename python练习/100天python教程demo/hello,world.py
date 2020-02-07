8415493f42d14fc367ae44a5537e3f52c1df90b5
lulu <844577557@qq.com>
2020-02-07 21:56:54 +0800

    1

--- python练习/100天python教程demo/hello,world.py
+++ python练习/100天python教程demo/hello,world.py
@@ -1,20 +1,18 @@
-""" 
-华氏度转换为摄氏度
+# 给出半径求园的周长和面积
+# 华氏度转换为摄氏度
+import math
 
 f = float(input('请输入华氏温度: '))
 c = (f - 32) / 1.8
-print('%.1f华氏度 = %.1f摄氏度' % (f, c)) """
+print('%.1f华氏度 = %.1f摄氏度' % (f, c))
 
-""" 给出半径求园的周长和面积
-import math
 radius = float(input('请输入园的半径: '))
 perimeter = 2 * math.pi * radius
 area = math.pi * radius * radius
 print('周长：%.2f' % perimeter)
-print('面积：%.2f' % area) """
+print('面积：%.2f' % area)
 
-""" # 输入年份判断是不是闰年
+# 输入年份判断是不是闰年
 year = int(input('请输入年份： '))
-is_leap = (year % 4 == 0 and year % 100 != 0 or
-           year % 400 == 0)
-print(is_leap) """
+is_leap = (year % 4 == 0 and year % 100 != 0 or year % 400 == 0)
+print(is_leap)
