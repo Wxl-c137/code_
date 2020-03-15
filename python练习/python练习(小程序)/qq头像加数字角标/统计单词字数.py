9b30a443a967d633f9d1f049ebeb18581155e706
wxl c137 <844577557@qq.com>
2020-03-15 15:43:58 +0800

    1

---
+++ python练习/python练习(小程序)/qq头像加数字角标/统计单词字数.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+# encoding:utf-8
+'''
+@File    :   统计单词字数.py
+@Time    :   2020/03/15 15:29:26
+@Author  :   WXL-c137
+@Version :   1.0
+@Contact :   844577557@qq.com
+@WebSite :   null
+'''
+# Start typing your code from here
