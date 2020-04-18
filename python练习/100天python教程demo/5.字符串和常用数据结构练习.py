abe8e4895fd45fcab9d2d10944969995a7b5637a
lulu <844577557qq.com>
2020-04-18 17:03:27 +0800

    2020_4_18_pc

--- python练习/100天python教程demo/5.字符串和常用数据结构练习.py
+++ python练习/100天python教程demo/5.字符串和常用数据结构练习.py
@@ -47,8 +47,7 @@ def get_suffix(filename, has_dot=False):
         return ''  """
 
 # 设计一个函数返回传入列表中最大和第二大的数
-"""  
-def max2(x):
+""" def max2(x):
     m1, m2 = (x[0], x[1] if x[0] > x[1] else (x[1], x[0]))
     for index in range(2, len(x)):
         if x[index] > m1:
@@ -56,12 +55,10 @@ def max2(x):
             m1 = x[index]
         elif x[index] > m2:
             m2 = x[index]
-    return m1, m2
- """
+    return m1, m2 """
 
-#  计算指定年月日是这一年的第几天
-
-year = int(input('请输入年份：'))
+#  计算指定年月日是这一年的第几天   TODO:recode
+""" year = int(input('请输入年份：'))
 month = int(input('请输入月份：'))
 day = int(input('请输入日期：'))
 
@@ -85,4 +82,53 @@ else:
     counts = day
     return counts
 
-print('这是%d年的第%d天'%(year,counts)
+print('这是%d年的第%d天'%(year,counts) """
+
+# 打印杨慧三角形 TODO:figure out the LSC
+""" def main():
+    num = int(input('Number of rows: '))
+    yh = [[]] * num
+    for row in range(len(yh)):
+        yh[row] = [None] * (row + 1)
+        for col in range(len(yh[row])):
+            if col == 0 or col == row:
+                yh[row][col] = 1
+            else:
+                yh[row][col] = yh[row - 1][col] + yh[row - 1][col - 1]
+            print(yh[row][col], end='\t')
+        print()
+
+
+if __name__ == '__main__':
+    main() """
+
+# 双色球选号
+""" from random import randint, sample
+
+
+def dispaly(balls):
+    for index, ball in enumerate(balls):
+        if index == len(balls) - 1:
+            print('|', end='')
+        print('%02d' % ball, end='')
+    print()
+
+
+def random_select():
+    red_balls = [x for x in range(1, 34)]
+    selected_balls = []
+    selected_balls = sample(red_balls, 6)
+    selected_balls.sort()
+    selected_balls.append(randint(1, 16))
+    return selected_balls
+
+
+def main():
+    n = int(input('机选几柱：'))
+    for _ in range(n):
+        dispaly(random_select())
+
+
+if __name__ == "__main__":
+    main()
+ """
--- python练习/100天python教程demo/tempCodeRunnerFile.py
+++ python练习/100天python教程demo/tempCodeRunnerFile.py
@@ -1,6 +1,20 @@
-import math
-radius = float(input('请输入园的半径: '))
-perimeter = 2 * math.pi * radius
-area = math.pi * radius * radius
-print('周长：%.2f' % perimeter)
-print('面积：%.2f' % area)
+if year % 4 == 0:
+    febdays = 29
+else:
+    febdays = 28
+return febdays
+
+month_list = [31, febdays, 31, 30, 31, 30, 31, 31, 30, 31, 31, 30]
+
+
+# def day_nums(month, day):
+if month > 1:
+    for i in range(month):
+        counts += month_list[i - 1]
+    coutns = counts + day
+    return counts
+else:
+    counts = day
+    return counts
+
+print('这是%d年的第%d天'%(year,counts)
