52eae7ab66051cffb240f2996d10591527c1d47e
lulu <844577557qq.com>
2020-04-18 15:30:42 +0800

    2020.4,18

--- python练习/100天python教程demo/5.字符串和常用数据结构练习.py
+++ python练习/100天python教程demo/5.字符串和常用数据结构练习.py
@@ -36,7 +36,8 @@ def generat_code(code_len=4):
 generat_code() """
 
 # 获取文件后缀的函数
-""" 
+"""
+
 def get_suffix(filename, has_dot=False):
     pos = filename.rfind('.')
     if 0 < pos < len(filename) - 1:
@@ -46,3 +47,42 @@ def get_suffix(filename, has_dot=False):
         return ''  """
 
 # 设计一个函数返回传入列表中最大和第二大的数
+"""  
+def max2(x):
+    m1, m2 = (x[0], x[1] if x[0] > x[1] else (x[1], x[0]))
+    for index in range(2, len(x)):
+        if x[index] > m1:
+            m2 = m1
+            m1 = x[index]
+        elif x[index] > m2:
+            m2 = x[index]
+    return m1, m2
+ """
+
+#  计算指定年月日是这一年的第几天
+
+year = int(input('请输入年份：'))
+month = int(input('请输入月份：'))
+day = int(input('请输入日期：'))
+
+# def leapyear(year):
+if year%4 == 0:
+    febdays = 29
+else:
+    febdays = 28
+return febdays
+
+month_list = [31, febdays, 31, 30, 31, 30, 31, 31, 30, 31, 31, 30]
+
+
+# def day_nums(month, day):
+if month > 1:
+    for i in range(month):
+        counts += month_list[i - 1]
+    coutns = counts + day
+    return counts
+else:
+    counts = day
+    return counts
+
+print('这是%d年的第%d天'%(year,counts)
